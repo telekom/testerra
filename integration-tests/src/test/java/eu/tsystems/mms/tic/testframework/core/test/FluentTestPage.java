17fd1bff03228d83697fff354e1cbc4346c05041
mreiche <mike.reiche@t-systems.com>
2019-10-24 16:27:56 +0200

    Added legacy examples for GuiElement instantiation

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/core/test/FluentTestPage.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/core/test/FluentTestPage.java
@@ -2,16 +2,24 @@ package eu.tsystems.mms.tic.testframework.core.test;
 
 import eu.tsystems.mms.tic.testframework.pageobjects.Check;
 import eu.tsystems.mms.tic.testframework.pageobjects.FluentPage;
+import eu.tsystems.mms.tic.testframework.pageobjects.GuiElement;
 import eu.tsystems.mms.tic.testframework.pageobjects.internal.facade.GuiElementFacade;
+import eu.tsystems.mms.tic.testframework.pageobjects.location.Locate;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class FluentTestPage extends FluentPage<FluentTestPage> {
 
     @Check
-    private GuiElementFacade visibleElement = findById("11");
+    private GuiElementFacade input = findById("11");
+    private GuiElementFacade input2 = new GuiElement(driver, By.id("11"));
 
     @Check
-    private GuiElementFacade subElement = findByQa("action/submit", visibleElement);
+    private GuiElementFacade button = findByQa("action/submit", input);
+    private GuiElementFacade button2 = input.getSubElement(Locate.by().qa("action/submit"));
+
+    private GuiElementFacade text = find(By.className("affe"));
+    private GuiElementFacade text2 = new GuiElement(driver, By.className("affe"));
 
     /**
      * Constructor for existing sessions.
@@ -23,11 +31,11 @@ public class FluentTestPage extends FluentPage<FluentTestPage> {
     }
 
     public GuiElementFacade input() {
-        return visibleElement;
+        return input;
     }
 
     public GuiElementFacade submit() {
-        return subElement;
+        return button;
     }
 
     @Override

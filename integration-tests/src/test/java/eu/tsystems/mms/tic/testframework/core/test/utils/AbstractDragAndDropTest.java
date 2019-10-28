d00d9fdd992ca38f812d05a1844442ce11023682
mreiche <mike.reiche@t-systems.com>
2019-10-28 15:01:20 +0100

    Cleanups

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/core/test/utils/AbstractDragAndDropTest.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/core/test/utils/AbstractDragAndDropTest.java
@@ -23,7 +23,6 @@ import eu.tsystems.mms.tic.testframework.AbstractTestSitesTest;
 import eu.tsystems.mms.tic.testframework.annotations.Fails;
 import eu.tsystems.mms.tic.testframework.core.test.TestPage;
 import eu.tsystems.mms.tic.testframework.pageobjects.GuiElement;
-import eu.tsystems.mms.tic.testframework.pageobjects.GuiElement;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;

58cf22a1b5ff70de0ad65bb88050c6eda4ff20bf
mreiche <mike.reiche@t-systems.com>
2019-12-09 14:22:57 +0100

    Merge fixes

--- driver-ui/src/main/java/eu/tsystems/mms/tic/testframework/pageobjects/GuiElement.java
+++ driver-ui/src/main/java/eu/tsystems/mms/tic/testframework/pageobjects/GuiElement.java
@@ -276,7 +276,6 @@ public class GuiElement implements
      * @return GuiElement
      */
     @Deprecated
-    @Override
     public GuiElement getSubElement(By by) {
         return getSubElement(by, null);
     }
@@ -291,7 +290,6 @@ public class GuiElement implements
      * @return GuiElement
      */
     @Deprecated
-    @Override
     public GuiElement getSubElement(By by, String description) {
         GuiElement subElement = getSubElement(Locate.by(by));
         if (description != null) subElement.setName(description);
--- driver-ui/src/main/java/eu/tsystems/mms/tic/testframework/webdrivermanager/WebDriverManagerUtils.java
+++ driver-ui/src/main/java/eu/tsystems/mms/tic/testframework/webdrivermanager/WebDriverManagerUtils.java
@@ -34,7 +34,6 @@ import eu.tsystems.mms.tic.testframework.exceptions.TesterraRuntimeException;
 import eu.tsystems.mms.tic.testframework.model.HostInfo;
 import eu.tsystems.mms.tic.testframework.model.NodeInfo;
 import eu.tsystems.mms.tic.testframework.report.model.BrowserInformation;
-import eu.tsystems.mms.tic.testframework.report.model.YauaaBrowserInformation;
 import eu.tsystems.mms.tic.testframework.report.model.context.SessionContext;
 import eu.tsystems.mms.tic.testframework.report.utils.ExecutionContextController;
 import org.json.JSONException;

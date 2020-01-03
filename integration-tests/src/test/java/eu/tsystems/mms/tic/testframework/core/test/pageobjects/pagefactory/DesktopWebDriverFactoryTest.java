3efe41e91a0471b013df7be2a3e490e8fc2680ee
mreiche <mike.reiche@t-systems.com>
2020-01-03 14:31:11 +0100

    Reorganized tests suites

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/core/test/pageobjects/pagefactory/DesktopWebDriverFactoryTest.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/core/test/pageobjects/pagefactory/DesktopWebDriverFactoryTest.java
@@ -17,7 +17,7 @@
  *     Peter Lehmann
  *     pele
  */
-package eu.tsystems.mms.tic.testframework.core.test.desktop;
+package eu.tsystems.mms.tic.testframework.core.test.pageobjects.pagefactory;
 
 import eu.tsystems.mms.tic.testframework.AbstractTest;
 import eu.tsystems.mms.tic.testframework.constants.Browsers;
@@ -26,7 +26,6 @@ import eu.tsystems.mms.tic.testframework.webdrivermanager.DesktopWebDriverReques
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverRequest;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.desktop.WebDriverMode;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
@@ -41,8 +40,8 @@ public class DesktopWebDriverFactoryTest extends AbstractTest {
     public void testT01_BaseURL() throws Exception {
         DesktopWebDriverRequest request = new DesktopWebDriverRequest();
         request.baseUrl = "http://google.de";
-        request.webDriverMode = WebDriverMode.local;
-        request.browser = Browsers.phantomjs;
+        //request.webDriverMode = WebDriverMode.local;
+        //request.browser = Browsers.phantomjs;
 
         WebDriver driver = WebDriverManager.getWebDriver(request);
         String currentUrl = driver.getCurrentUrl();
@@ -52,20 +51,20 @@ public class DesktopWebDriverFactoryTest extends AbstractTest {
     @Test
     public void testT02_BaseURL_NotSet() throws Exception {
         DesktopWebDriverRequest request = new DesktopWebDriverRequest();
-        request.webDriverMode = WebDriverMode.local;
-        request.browser = Browsers.phantomjs;
+        //request.webDriverMode = WebDriverMode.local;
+        //request.browser = Browsers.phantomjs;
 
         WebDriver driver = WebDriverManager.getWebDriver(request);
         String currentUrl = driver.getCurrentUrl();
-        Assert.assertTrue(currentUrl.contains("192.168"), "Current URL contains local ip - actual: " + currentUrl);
+        Assert.assertTrue(currentUrl.contains("localhost"), "Current URL is invalid - actual: " + currentUrl);
     }
 
     @Test
     public void testT03_EndPointCapabilities() throws Exception {
         DesktopWebDriverRequest request = new DesktopWebDriverRequest();
         request.baseUrl = "http://google.de";
-        request.webDriverMode = WebDriverMode.local;
-        request.browser = Browsers.phantomjs;
+        //request.webDriverMode = WebDriverMode.local;
+        //request.browser = Browsers.phantomjs;
 
         /*
         create caps

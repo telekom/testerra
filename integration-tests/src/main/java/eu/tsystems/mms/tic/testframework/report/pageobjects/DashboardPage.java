293dc8c44617589ed94bf8bcea6a4ef6f92a6692
Eric Kubenka <Eric.Kubenka@t-systems.com>
2020-01-15 07:48:28 +0100

    : Fixing test testT01_checkAllNumbersOfOneTest

--- integration-tests/src/main/java/eu/tsystems/mms/tic/testframework/report/pageobjects/DashboardPage.java
+++ integration-tests/src/main/java/eu/tsystems/mms/tic/testframework/report/pageobjects/DashboardPage.java
@@ -7,17 +7,22 @@ import eu.tsystems.mms.tic.testframework.report.abstracts.AbstractReportPage;
 import eu.tsystems.mms.tic.testframework.report.model.IReportAnnotationVerifier;
 import eu.tsystems.mms.tic.testframework.report.model.ReportAnnotationType;
 import eu.tsystems.mms.tic.testframework.report.model.TestResultHelper;
-import eu.tsystems.mms.tic.testframework.report.pageobjects.dashboard.modules.*;
+import eu.tsystems.mms.tic.testframework.report.pageobjects.dashboard.modules.DashboardModuleClassBarChart;
+import eu.tsystems.mms.tic.testframework.report.pageobjects.dashboard.modules.DashboardModuleFailureCorridor;
+import eu.tsystems.mms.tic.testframework.report.pageobjects.dashboard.modules.DashboardModuleHistoryAreaChart;
+import eu.tsystems.mms.tic.testframework.report.pageobjects.dashboard.modules.DashboardModuleInformationCorridor;
+import eu.tsystems.mms.tic.testframework.report.pageobjects.dashboard.modules.DashboardModuleMethodChart;
+import eu.tsystems.mms.tic.testframework.report.pageobjects.dashboard.modules.DashboardModuleTestResultNumberBreakdown;
+import eu.tsystems.mms.tic.testframework.report.pageobjects.dashboard.modules.DashboardModuleTestResultPieChart;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 
 /**
  * Created by riwa on 25.10.2016.
  */
 public class DashboardPage extends AbstractReportPage implements IReportAnnotationVerifier {
+
     @Check
     public final GuiElement testerraLogo = new GuiElement(this.driver, By.cssSelector("img[alt='testerra']"), mainFrame);
     public final DashboardModuleTestResultPieChart dashboardModuleTestResultPieChart = PageFactory.create(DashboardModuleTestResultPieChart.class, driver);
@@ -100,7 +105,7 @@ public class DashboardPage extends AbstractReportPage implements IReportAnnotati
      */
     public GuiElement getResultTableHeaderForTestResult(TestResultHelper.TestResult testResult) {
         String testState = testResult.getTestState();
-        GuiElement testResultTableHeader = new GuiElement(driver, By.xpath("(//*[@class='header']/../tr[@class='test testbar"+testState+" filterMethods filter"+testState+"'])[1]"), mainFrame);
+        GuiElement testResultTableHeader = new GuiElement(driver, By.xpath("(//*[@class='header']/../tr[@class='test testbar" + testState + " filterMethods filter" + testState + "'])[1]"), mainFrame);
         testResultTableHeader.setName("testResultTableHeader");
 
         return testResultTableHeader;
@@ -116,7 +121,7 @@ public class DashboardPage extends AbstractReportPage implements IReportAnnotati
      * Check for the visibility of a annotation type for a method name.
      *
      * @param annotationType the type to be checked.
-     * @param methodName a given method which is assumed to be annotated.
+     * @param methodName     a given method which is assumed to be annotated.
      */
     @Override
     public void assertAnnotationMarkIsDisplayed(ReportAnnotationType annotationType, String methodName) {
@@ -153,7 +158,7 @@ public class DashboardPage extends AbstractReportPage implements IReportAnnotati
         return PageFactory.create(MethodDetailsPage.class, this.driver);
     }
 
-    public MethodDetailsPage goToMethodDetailsPageUsingTestNumberFilter(TestResultHelper.TestResult testResult, String className, String methodName) throws Exception{
+    public MethodDetailsPage goToMethodDetailsPageUsingTestNumberFilter(TestResultHelper.TestResult testResult, String className, String methodName) throws Exception {
         dashboardModuleTestResultPieChart.clickActualRunPieSegmentForTestResult(testResult);
         GuiElement barChartElement = dashboardModuleClassBarChart.getBarChartElementByClassName(className);
         barChartElement.click();

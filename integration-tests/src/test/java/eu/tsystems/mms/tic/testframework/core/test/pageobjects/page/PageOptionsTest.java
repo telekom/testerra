ee9ee8563b0d7d0ea8edd2df90992b25e7e7f36b
mreiche <mike.reiche@t-systems.com>
2020-01-17 13:58:28 +0100

    Renamed to real PageOptionsTest

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/core/test/pageobjects/page/PageOptionsTest.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/core/test/pageobjects/page/PageOptionsTest.java
@@ -2,7 +2,7 @@ package eu.tsystems.mms.tic.testframework.core.test.pageobjects.page;
 
 import eu.tsystems.mms.tic.testframework.pageobjects.PageWithPageOptions;
 
-public class PageOptionsTest_IoC extends PageOptionsTest_Deprecated {
+public class PageOptionsTest extends PageOptionsTest_Deprecated {
 
     @Override
     public PageWithPageOptions getPage() {

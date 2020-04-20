487d65c0d7e1ffc32cafa3336fb7149ace420603
mreiche <mike.reiche@t-systems.com>
2020-04-20 11:10:10 +0200

    Added test groups

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/report/test/ReportExportTest.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/report/test/ReportExportTest.java
@@ -2,6 +2,7 @@ package eu.tsystems.mms.tic.testframework.report.test;
 
 import eu.tsystems.mms.tic.testframework.common.PropertyManager;
 import eu.tsystems.mms.tic.testframework.report.general.ReportDirectory;
+import eu.tsystems.mms.tic.testframework.report.general.SystemTestsGroup;
 import eu.tsystems.mms.tic.testframework.report.model.context.report.Report;
 import eu.tsystems.mms.tic.testframework.testing.TesterraTest;
 import org.testng.Assert;
@@ -11,13 +12,13 @@ import java.io.File;
 
 public class ReportExportTest extends TesterraTest {
 
-    @Test
+    @Test(groups = {SystemTestsGroup.SYSTEMTESTSFILTER1})
     public void test_TestNG_XML() {
         File testNGExportFile = new File(PropertyManager.getProperty(ReportDirectory.REPORT_DIRECTORY_1.getReportDirectory()) + "/" + Report.XML_FOLDER_NAME + "/testng-results.xml");
         Assert.assertTrue(testNGExportFile.exists());
     }
 
-    @Test
+    @Test(groups = {SystemTestsGroup.SYSTEMTESTSFILTER1})
     public void test_JUnit_XML() {
         File testNGExportFile = new File(PropertyManager.getProperty(ReportDirectory.REPORT_DIRECTORY_1.getReportDirectory()) + "/" + Report.XML_FOLDER_NAME + "/TEST-Results.xml");
         Assert.assertTrue(testNGExportFile.exists());

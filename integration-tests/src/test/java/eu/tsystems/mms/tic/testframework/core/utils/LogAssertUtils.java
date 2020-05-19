64657c2c8209db443e3c6aa217b23f3ffa1a8314
Eric Kubenka <Eric.Kubenka@t-systems.com>
2020-05-19 07:50:55 +0200

    : Simple Header.

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/core/utils/LogAssertUtils.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/core/utils/LogAssertUtils.java
@@ -1,9 +1,18 @@
 /*
- * Created on 14.03.2012
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Copyright(c) 2011 - 2011 T-Systems Multimedia Solutions GmbH
- * Riesaer Str. 5, 01129 Dresden
- * All rights reserved.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Eric Kubenka
  */
 package eu.tsystems.mms.tic.testframework.core.utils;
 

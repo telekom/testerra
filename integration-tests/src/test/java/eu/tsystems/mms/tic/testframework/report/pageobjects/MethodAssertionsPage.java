26c1bf8e217a760378c658c8aede173804ea6ccb
Alex Rockstroh <Alex.Rockstroh@t-systems.com>
2020-06-08 13:02:12 +0200

    : small code improvements based on pull-request comments

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/report/pageobjects/MethodAssertionsPage.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/report/pageobjects/MethodAssertionsPage.java
@@ -4,9 +4,10 @@ import org.openqa.selenium.WebDriver;
 
 public class MethodAssertionsPage extends MethodDetailsPage {
 
+    //TODO add @check annotation after bug ticket 966 is solved
+
     public MethodAssertionsPage(WebDriver driver) {
 
         super(driver);
-        checkPage();
     }
 }

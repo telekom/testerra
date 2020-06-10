be28cdbbfe5808360d53fae5bcadeb08893b804d
Eric Kubenka <Eric.Kubenka@t-systems.com>
2020-06-10 08:36:34 +0200

    : Added Fileheaders according to telekom/testerra for core-interop

--- core-interop/src/main/java/eu/tsystems/mms/tic/testframework/common/UTF8ResourceBundleControl.java
+++ core-interop/src/main/java/eu/tsystems/mms/tic/testframework/common/UTF8ResourceBundleControl.java
@@ -1,3 +1,24 @@
+/*
+ * Testerra
+ *
+ * (C) 2020, Eric Kubenka, T-Systems Multimedia Solutions GmbH, Deutsche Telekom AG
+ *
+ * Deutsche Telekom AG and all other contributors /
+ * copyright owners license this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 package eu.tsystems.mms.tic.testframework.common;
 
 import java.io.IOException;
@@ -13,7 +34,7 @@ import java.util.ResourceBundle;
  * Allows to load localized resource bundles in UTF8 encoding
  * You can change the language by the Java environment variable
  *   -Duser.language=en
- * @see https://stackoverflow.com/questions/4659929/how-to-use-utf-8-in-resource-properties-with-resourcebundle
+ * https://stackoverflow.com/questions/4659929/how-to-use-utf-8-in-resource-properties-with-resourcebundle
  */
 public class UTF8ResourceBundleControl extends ResourceBundle.Control {
     public ResourceBundle newBundle

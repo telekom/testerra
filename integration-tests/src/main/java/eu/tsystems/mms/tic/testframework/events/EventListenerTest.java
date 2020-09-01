78e9d275cc47835d5eb5abf39fcfb33661739efd
mreiche <mike.reiche@t-systems.com>
2020-09-01 11:08:33 +0200

    Added Test for Event Listener implementation

---
+++ integration-tests/src/main/java/eu/tsystems/mms/tic/testframework/events/EventListenerTest.java
@@ -0,0 +1,72 @@
+package eu.tsystems.mms.tic.testframework.events;
+
+import com.google.common.eventbus.Subscribe;
+import eu.tsystems.mms.tic.testframework.report.TesterraListener;
+import eu.tsystems.mms.tic.testframework.testing.TesterraTest;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.testng.Assert;
+import org.testng.IMethodInstance;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class EventListenerTest extends TesterraTest {
+
+    public class MyListener implements
+            MethodStartEvent.Listener,
+            MethodEndEvent.Listener,
+            InterceptMethodsEvent.Listener
+    {
+
+        public MethodStartEvent startEvent;
+        public MethodEndEvent endEvent;
+
+        @Override
+        @Subscribe
+        public void onMethodEnd(MethodEndEvent event) {
+            endEvent = event;
+        }
+
+        @Override
+        @Subscribe
+        public void onMethodStart(MethodStartEvent event) {
+            startEvent = event;
+        }
+
+        @Override
+        @Subscribe
+        public void onInterceptMethods(InterceptMethodsEvent event) {
+            List<IMethodInstance> testToBeIgnored = event.getMethodInstances().stream()
+                    .filter(iMethodInstance -> !iMethodInstance.getMethod().getMethodName().equals("testToBeIgnored"))
+                    .collect(Collectors.toList());
+            event.setMethodInstances(testToBeIgnored);
+        }
+    }
+
+    private MyListener listener;
+
+    @BeforeSuite
+    public void setupListener() {
+        listener = new MyListener();
+        TesterraListener.getEventBus().register(listener);
+    }
+
+    @Test
+    public void testToBeIgnored() {
+        Assert.assertTrue(false, "This test should be filtered and never be called");
+    }
+
+    @Test
+    public void testCalled() {
+        // I just do nothing, sorry
+    }
+
+    @AfterClass
+    public void checkListener() {
+        Assert.assertNotNull(listener);
+        Assert.assertNotNull(listener.startEvent);
+        Assert.assertNotNull(listener.endEvent);
+        TesterraListener.getEventBus().unregister(listener);
+    }
+}

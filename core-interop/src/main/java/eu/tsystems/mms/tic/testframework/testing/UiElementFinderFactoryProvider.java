fbc131d69360c0e31e34f5fd4877090b16cd9bba
mreiche <mike.reiche@t-systems.com>
2020-09-28 00:45:20 +0200

    Updated doc

--- core-interop/src/main/java/eu/tsystems/mms/tic/testframework/testing/UiElementFinderFactoryProvider.java
+++ core-interop/src/main/java/eu/tsystems/mms/tic/testframework/testing/UiElementFinderFactoryProvider.java
@@ -22,11 +22,10 @@
 package eu.tsystems.mms.tic.testframework.testing;
 
 import eu.tsystems.mms.tic.testframework.common.Testerra;
-import eu.tsystems.mms.tic.testframework.pageobjects.internal.UiElementFinder;
 import eu.tsystems.mms.tic.testframework.pageobjects.internal.UiElementFinderFactory;
 
 /**
- * Provides a {@link UiElementFinder}
+ * Provides a {@link UiElementFinderFactory}
  * @author Mike Reiche
  */
 public interface UiElementFinderFactoryProvider {

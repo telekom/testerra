c67f9946f1e39b32f9f2791f85954caeb66766c6
Hannes Jörk <hannes.joerk@t-systems.com>
2020-10-01 14:41:29 +0200

    tried implementing the apex chart

--- report-ng/app/src/components/dashboard.ts
+++ report-ng/app/src/components/dashboard.ts
@@ -3,30 +3,37 @@ import {DataLoader} from "../services/data-loader";
 import {data} from "../services/report-model";
 import {autoinject} from "aurelia-framework";
 import {StatusConverter} from "../services/status-converter";
+import 'apexcharts'
 import IExecutionContext = data.IExecutionContext;
 import ResultStatusType = data.ResultStatusType;
 
 @autoinject()
 export class Dashboard {
-  _chartData : object;
+  private _apexDonutOptions: any = undefined;
+  private _testsPassed: number = 0;
+  private _testsFailed: number = 0;
+  private _testsSkipped: number = 0;
+
   _executionContext:IExecutionContext;
-  _testsPassed:number = 0;
 
   constructor(
     private _dataLoader:DataLoader,
     private _statusConverter:StatusConverter
   ) {
   }
+
+
   attached() {
+    this._prepareDonutChart();
+
     this._dataLoader.getExecutionAggregate().then(executionAggregate => {
       console.log(executionAggregate);
       executionAggregate.testContexts.forEach(testContext => {
         testContext.classContextIds.forEach(classContextId => {
           this._dataLoader.getClassContextAggregate(classContextId).then(classContextAggregate => {
-            console.log(classContextAggregate);
-
             classContextAggregate.methodContexts.forEach(methodContext => {
               const status = this._statusConverter.groupStatisticStatus(methodContext.contextValues.resultStatus);
+              //console.log("status:" + status + "result status" + resultStatus);
               if (status == ResultStatusType.PASSED) {
                 this._testsPassed++;
               }
@@ -35,20 +42,45 @@ export class Dashboard {
         })
       });
     });
-
-    this._chartData = {
-      chart: {
-        type: 'bar'
-      },
-      series: [
-        {
-          name: 'sales',
-          data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-        }
-      ],
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-      }
-    }
+    console.log(this._testsPassed);
   }
+
+  private _prepareData(resultStatus: ResultStatusType): number {
+    let testsInState: number = 0;
+
+    this._dataLoader.getExecutionAggregate().then(executionAggregate => {
+      console.log(executionAggregate);
+      executionAggregate.testContexts.forEach(testContext => {
+        testContext.classContextIds.forEach(classContextId => {
+          this._dataLoader.getClassContextAggregate(classContextId).then(classContextAggregate => {
+            classContextAggregate.methodContexts.forEach(methodContext => {
+              const status = this._statusConverter.groupStatisticStatus(methodContext.contextValues.resultStatus);
+              //console.log("status:" + status + "result status" + resultStatus);
+              if (status == resultStatus) {
+                testsInState++;
+              }
+            });
+          });
+        })
+      });
+    });
+    console.log(testsInState);
+    return testsInState;
+  }
+
+  private _prepareDonutChart(): void {
+    let options: any = undefined;
+
+    options = {
+      chart: {
+        type: 'donut'
+      },
+      series: [10,10,20],
+      labels: ["passed", "failed", "skipped"]
+    }
+
+    this._apexDonutOptions = options;
+  }
+
+
 }

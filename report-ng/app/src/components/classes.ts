2b2e2b6ee323c054a71fdfec8611c80e313bc0bf
mreiche <mike.reiche@t-systems.com>
2020-10-02 12:41:26 +0200

    Simplified loading of class statistics

--- report-ng/app/src/components/classes.ts
+++ report-ng/app/src/components/classes.ts
@@ -2,58 +2,29 @@ import {data} from "../services/report-model";
 import {DataLoader} from "../services/data-loader";
 import {StatusConverter} from "../services/status-converter";
 import {autoinject} from "aurelia-framework";
-import ResultStatusType = data.ResultStatusType;
+import {StatisticsGenerator} from "../services/statistics-generator";
+import {ClassStatistics} from "../services/statistic-models";
 
 @autoinject()
 export class Classes {
-  _testsClasses: string;
-  _methodsId;
-  _status:string;
-
+  private _classStatistics:ClassStatistics[] = [];
 
   constructor(
     private _dataLoader:DataLoader,
-    private _statusConverter:StatusConverter
+    private _statusConverter:StatusConverter,
+    private _statisticsGenerator:StatisticsGenerator,
 
   ) {
   }
   attached() {
-    this._dataLoader.getExecutionAggregate().then(executionAggregate => {
-      console.log(executionAggregate);
-      executionAggregate.testContexts.forEach(testContext => {
-        testContext.classContextIds.forEach(classContextId => {
-          this._dataLoader.getClassContextAggregate(classContextId).then(classContextAggregate => {
-            console.log(classContextAggregate);
-
-            this._methodsId= classContextAggregate.classContext.methodContextIds ;
-            this._testsClasses= classContextAggregate.classContext.contextValues.name;
-
-          });
-        })
-      });
+    this._statisticsGenerator.getExecutionStatistics().then(executionStatistics => {
+      executionStatistics.classStatistics.forEach(classStatistics => {
+        console.log(classStatistics);
+        this._classStatistics.push(classStatistics);
+        // classStatistics.classAggregate.methodContexts.forEach(methodContext => {
+        //   this._methods.push(methodContext);
+        // })
+      })
     });
-
-  }
-
-  //dessert test
-  ice:dessert = new dessert('ice',5,50,5,'nice');
-  Eclair:dessert = new dessert('Eclair',30,20,0,'meh');
-  desserts: dessert[] =  [this.ice, this.Eclair] ;
-
-}
-
-
-class dessert{
-  name: string;
-  calories: number;
-  carbs: number;
-  protein: number;
-  comment: string;
-  constructor(name, calories, carbs, protein, comment) {
-    this.name =  name;
-    this.calories = calories;
-    this.carbs = carbs;
-    this.protein = protein;
-    this.comment = comment;
   }
 }

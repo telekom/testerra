d143a299a8c4d6c10a1d545f0a03a1b3e72dce80
Hannes Jörk <hannes.joerk@t-systems.com>
2020-10-08 14:32:32 +0200

    used moment-duration-format for improved duration formatting

--- report-ng/app/src/components/dashboard/dashboard.ts
+++ report-ng/app/src/components/dashboard/dashboard.ts
@@ -5,27 +5,31 @@ import {autoinject} from "aurelia-framework";
 import {StatusConverter} from "../../services/status-converter";
 import {StatisticsGenerator} from "../../services/statistics-generator";
 import {ExecutionStatistics} from "../../services/statistic-models";
+import moment from 'moment';
+import 'moment-duration-format';
 import IExecutionContext = data.IExecutionContext;
 
 @autoinject()
 export class Dashboard {
   private _apexDonutOptions: any = undefined;
+  private _testDuration: string = undefined;
 
-  _executionContext:IExecutionContext;
-  private _executionStatistics:ExecutionStatistics;
+
+  _executionContext: IExecutionContext;
+  private _executionStatistics: ExecutionStatistics;
 
   constructor(
-    private _statusConverter:StatusConverter,
-    private _statisticsGenerator:StatisticsGenerator,
+    private _statusConverter: StatusConverter,
+    private _statisticsGenerator: StatisticsGenerator,
   ) {
   }
 
   attached() {
-
     this._statisticsGenerator.getExecutionStatistics().then(executionStatistics => {
       this._executionStatistics = executionStatistics;
       this._executionContext = executionStatistics.executionAggregate.executionContext;
-      console.log(executionStatistics);
+      this._testDuration = moment.duration(<number>this._executionContext.contextValues.endTime - <number>this._executionContext.contextValues.startTime).format();
+      console.log(this._executionContext);
       this._prepareDonutChart(executionStatistics);
     })
   }

9712609d23cf2bc224ab36b0aaabfbca2c0d9a87
Hannes Jörk <hannes.joerk@t-systems.com>
2020-10-13 10:54:32 +0200

    reverted to old duration formatting

--- report-ng/app/src/components/dashboard/dashboard.ts
+++ report-ng/app/src/components/dashboard/dashboard.ts
@@ -7,21 +7,15 @@ import {StatisticsGenerator} from "../../services/statistics-generator";
 import {ExecutionStatistics} from "../../services/statistic-models";
 import IExecutionContext = data.IExecutionContext;
 import moment from "moment";
+import 'moment-duration-format';
 
 @autoinject()
 export class Dashboard {
   private _apexDonutOptions: any = undefined;
-  // dur =moment.duration(5000);
-  // dur =moment.duration(1,"day");
-//dur= moment.duration(2,"day").add(1,"week").humanize(); //9day
-//dur= moment.duration(2,"day").subtract(1,"week").humanize(); //5days
-  //private dur =moment().format('MMMM Do YYYY, h:mm:ss a');
+  private _testDuration: string = undefined;
 
   private _executionContext:IExecutionContext;
   private _executionStatistics:ExecutionStatistics;
-  private _startDate;
-  private _endDate;
-  private _duration;
 
   constructor(
     private _statusConverter:StatusConverter,
@@ -34,13 +28,10 @@ export class Dashboard {
     this._statisticsGenerator.getExecutionStatistics().then(executionStatistics => {
       this._executionStatistics = executionStatistics;
       this._executionContext = executionStatistics.executionAggregate.executionContext;
-      console.log(executionStatistics);
+      this._testDuration = moment.duration(<number>this._executionContext.contextValues.endTime - <number>this._executionContext.contextValues.startTime).format();
+      console.log(this._executionContext);
       this._prepareDonutChart(executionStatistics);
 
-      //duration
-      this._startDate= this._executionContext.contextValues.startTime;
-      this._endDate= this._executionContext.contextValues.endTime;
-      this._duration=moment(this._endDate).from(this._startDate);
     })
   }
 

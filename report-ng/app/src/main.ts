c654dc1d77021ab4cce84ea591b82e3cdb141919
mreiche <mike.reiche@t-systems.com>
2020-11-16 09:54:48 +0100

    Optimized imports and final application size

--- report-ng/app/src/main.ts
+++ report-ng/app/src/main.ts
@@ -8,8 +8,21 @@ export function configure(aurelia: Aurelia) {
 
     aurelia.use
         .standardConfiguration()
-        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/all'))
+        //.plugin(PLATFORM.moduleName('@aurelia-mdc-web/all'))
+        // .plugin(PLATFORM.moduleName('@aurelia-mdc-web/button'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/card'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/data-table'))
         .plugin(PLATFORM.moduleName('@aurelia-mdc-web/dialog'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/drawer'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/elevation'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/form-field'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/layout-grid'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/ripple'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/select'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/switch'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/text-field'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/top-app-bar'))
+        .plugin(PLATFORM.moduleName('@aurelia-mdc-web/typography'))
         .globalResources([
             PLATFORM.moduleName('components/apex-chart/apex-chart'),
             PLATFORM.moduleName('t-systems-aurelia-components/src/value-converters/date-format-value-converter'),

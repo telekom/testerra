572b57f149393eed790bf5c89af43c64bcca651f
Hannes Jörk <hannes.joerk@t-systems.com>
2020-12-01 14:20:33 +0100

    added data from methods to timeline

--- report-ng/app/src/components/threads/threads.ts
+++ report-ng/app/src/components/threads/threads.ts
@@ -6,6 +6,8 @@ import {StatusConverter} from "../../services/status-converter";
 import {FailureAspectStatistics} from "../../services/statistic-models";
 import { DataSet, Timeline } from "vis-timeline/standalone";
 import "vis-timeline/styles/vis-timeline-graph2d.css";
+import {data} from "../../services/report-model";
+import MethodContext = data.MethodContext;
 
 @autoinject()
 export class Threads extends AbstractViewModel {
@@ -38,28 +40,37 @@ export class Threads extends AbstractViewModel {
             })
             this._loading = false;
             this.updateUrl(this.queryParams);
-            this._prepareTimelineData()
+            this._prepareTimelineData(executionStatistics.executionAggregate.methodContexts)
         });
     }
 
-    _prepareTimelineData() {
+    _prepareTimelineData(methodContexts) {
         // DOM element where the Timeline will be attached
         const container = document.getElementById("container");
+        let data: Array<Object> = [];
 
-// Create a DataSet (allows two way data-binding)
-        const items = new DataSet([
-            { id: 1, content: "item 1", start: "2014-04-20" },
-            { id: 2, content: "item 2", start: "2014-04-14" },
-            { id: 3, content: "item 3", start: "2014-04-18" },
-            { id: 4, content: "item 4", start: "2014-04-16", end: "2014-04-19" },
-            { id: 5, content: "item 5", start: "2014-04-25" },
-            { id: 6, content: "item 6", start: "2014-04-27", type: "point" }
-        ]);
+        console.log(methodContexts);
 
-// Configuration for the Timeline
+        methodContexts.forEach(function(context: MethodContext, index) {
+            let thread: Object;
+            thread = {
+                id: index,
+                content: context.contextValues.name,
+                start: context.contextValues.startTime,
+                end: context.contextValues.endTime
+            };
+            data.push(thread);
+        })
+
+        // Create a DataSet (allows two way data-binding)
+        const items = new DataSet(data);
+
+        console.log("items: ", items);
+
+        // Configuration for the Timeline
         const options = {};
 
-// Create a Timeline
+        // Create a Timeline
         const timeline = new Timeline(container, items, options);
     }
 }

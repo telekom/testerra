23746c6a3c7c1ea49b4da3565746b61b10f26790
mreiche <mike.reiche@t-systems.com>
2021-02-26 11:45:22 +0100

    Fixed creating failure aspect on irrelevant cause stack

--- report-ng/app/src/services/statistic-models.ts
+++ report-ng/app/src/services/statistic-models.ts
@@ -253,20 +253,29 @@ export class FailureAspectStatistics extends Statistics {
         private _errorContext:IErrorContext
     ) {
         super();
+        console.log(this._errorContext);
         this.relevantCause = this._findRelevantCause(this._errorContext.stackTrace);
         if (this._errorContext.description) {
             this.identifier = this._errorContext.description;
             this.message = this._errorContext.description;
-        } else {
+        } else if (this.relevantCause) {
             this.message = this.relevantCause.message?.trim();
             this.identifier = this.relevantCause.className + this.message;
         }
     }
 
+    /**
+     * This method finds a relevant cause from the stack trace.
+     * If it could not find any, it takes the first cause from the stack.
+     */
     private _findRelevantCause(causes:IStackTraceCause[]):IStackTraceCause {
-        return causes.find(cause => {
+        let relevantCause = causes.find(cause => {
             return !this._irrelevantClassNameNeedles.find(needle => cause.className.indexOf(needle) >= 0);
         })
+        if (!relevantCause) {
+            relevantCause = causes.find(value => true);
+        }
+        return relevantCause;
     }
 
     addMethodContext(methodContext:IMethodContext) {

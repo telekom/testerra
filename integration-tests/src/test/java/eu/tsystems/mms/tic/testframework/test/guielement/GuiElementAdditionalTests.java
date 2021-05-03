0cb5103b710e693a63aee69623b57d82ea21ea0c
Mike Reiche <mike.reiche@t-systems.com>
2021-05-03 17:45:55 +0200

    Added test for unique sub element selectors via. configurator.

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/test/guielement/GuiElementAdditionalTests.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/test/guielement/GuiElementAdditionalTests.java
@@ -24,11 +24,14 @@ package eu.tsystems.mms.tic.testframework.test.guielement;
 
 import eu.tsystems.mms.tic.testframework.AbstractTestSitesTest;
 import eu.tsystems.mms.tic.testframework.core.utils.LogAssertUtils;
+import eu.tsystems.mms.tic.testframework.exceptions.ElementNotFoundException;
+import eu.tsystems.mms.tic.testframework.exceptions.TimeoutException;
 import eu.tsystems.mms.tic.testframework.pageobjects.GuiElement;
 import eu.tsystems.mms.tic.testframework.pageobjects.Locate;
 import eu.tsystems.mms.tic.testframework.utils.FileUtils;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
 import java.io.File;
+import java.sql.Time;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -75,11 +78,22 @@ public class GuiElementAdditionalTests extends AbstractTestSitesTest {
     /**
      *
      */
-    @Test
-    public void test04_LocateWithUniqueConfigurator_fails() {
+    @Test()
+    public void test04_LocateSubElementWithUniqueConfigurator_fails() {
         Locate.setConfigurator(Locate::unique);
         final WebDriver driver = WebDriverManager.getWebDriver();
-        GuiElement div = new GuiElement(driver, By.id("//div"));
-        div.asserts().assertIsDisplayed();
+        GuiElement body = new GuiElement(driver, By.xpath("//body"));
+        GuiElement div = body.getSubElement(By.xpath("//div"));
+
+        Throwable notFoundException = null;
+        try {
+            div.click();
+        } catch (TimeoutException e) {
+            notFoundException = e.getCause();
+        } catch (ElementNotFoundException e) {
+            notFoundException = e;
+        }
+
+        Assert.assertTrue(notFoundException.getCause().getMessage().contains("To many WebElements found"), "Expect to fail because of to many WebElements found");
     }
 }

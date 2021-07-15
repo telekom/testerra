71ad3809cad21ebf93c347eb9152d0d2e4831bab
Mike Reiche <mike.reiche@t-systems.com>
2021-07-15 09:31:50 +0200

    DesktopWebDriverUtils is now just a wrapper interface to deprecated methods. Fixes #93

--- driver-ui-desktop/src/main/java/eu/tsystems/mms/tic/testframework/webdrivermanager/DesktopWebDriverUtils.java
+++ driver-ui-desktop/src/main/java/eu/tsystems/mms/tic/testframework/webdrivermanager/DesktopWebDriverUtils.java
@@ -23,8 +23,6 @@
 
 import eu.tsystems.mms.tic.testframework.logging.Loggable;
 import eu.tsystems.mms.tic.testframework.pageobjects.GuiElement;
-import eu.tsystems.mms.tic.testframework.pageobjects.internal.frames.FrameLogic;
-import eu.tsystems.mms.tic.testframework.utils.JSUtils;
 
 public final class DesktopWebDriverUtils implements Loggable {
 
@@ -32,57 +30,27 @@ public final class DesktopWebDriverUtils implements Loggable {
 
     }
 
-    private void inFrame(GuiElement guiElement, Runnable runnable) {
-        FrameLogic frameLogic = guiElement.getFrameLogic();
-        if (frameLogic != null) {
-            frameLogic.switchToCorrectFrame();
-        }
-        runnable.run();
-        if (frameLogic != null) {
-            frameLogic.switchToDefaultFrame();
-        }
-    }
-
     public void clickAbsolute(GuiElement guiElement) {
-        log().trace("Absolute navigation and click on: " + guiElement.toString());
-        JSUtils utils = new JSUtils();
-        inFrame(guiElement, () -> {
-            utils.clickAbsolute(guiElement.getWebDriver(), guiElement.getWebElement());
-        });
+        guiElement.clickAbsolute();
     }
 
     public void mouseOverAbsolute2Axis(GuiElement guiElement) {
-        JSUtils utils = new JSUtils();
-        inFrame(guiElement, () -> {
-            utils.mouseOverAbsolute2Axis(guiElement.getWebDriver(), guiElement.getWebElement());
-        });
+        guiElement.mouseOverAbsolute2Axis();
     }
 
     public void mouseOverJS(GuiElement guiElement) {
-        JSUtils utils = new JSUtils();
-        inFrame(guiElement, () -> {
-            utils.mouseOver(guiElement.getWebDriver(), guiElement.getWebElement());
-        });
+        guiElement.mouseOverJS();
     }
 
     public void clickJS(GuiElement guiElement) {
-        JSUtils utils = new JSUtils();
-        inFrame(guiElement, () -> {
-            utils.click(guiElement.getWebDriver(), guiElement.getWebElement());
-        });
+        guiElement.clickJS();
     }
 
     public void rightClickJS(GuiElement guiElement) {
-        JSUtils utils = new JSUtils();
-        inFrame(guiElement, () -> {
-            utils.rightClick(guiElement.getWebDriver(), guiElement.getWebElement());
-        });
+        guiElement.rightClickJS();
     }
 
     public void doubleClickJS(GuiElement guiElement) {
-        JSUtils utils = new JSUtils();
-        inFrame(guiElement, () -> {
-            utils.doubleClick(guiElement.getWebDriver(), guiElement.getWebElement());
-        });
+        guiElement.doubleClickJS();
     }
 }

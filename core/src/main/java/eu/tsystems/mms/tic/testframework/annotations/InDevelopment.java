b4fd57be349d97493acae871e89cec982bb516cc
Mike Reiche <mike.reiche@t-systems.com>
2021-09-15 15:28:28 +0200

    Marked some orphaned annotations as @deprecated

--- core/src/main/java/eu/tsystems/mms/tic/testframework/annotations/InDevelopment.java
+++ core/src/main/java/eu/tsystems/mms/tic/testframework/annotations/InDevelopment.java
@@ -33,6 +33,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+@Deprecated
 public @interface InDevelopment {
 
     String color() default "#a7a5a5";

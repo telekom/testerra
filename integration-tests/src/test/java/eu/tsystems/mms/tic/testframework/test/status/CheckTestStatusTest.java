c5eefbaff47352317915ab4fcc48331814a9ccdc
Clemens Grosse <Clemens.Grosse@t-systems.com>
2022-01-05 15:10:47 +0100

    updated CheckTestStatusTest and log4j2-pretest.xml; added CheckStatus.xml in Integration.xml;

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/test/status/CheckTestStatusTest.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/test/status/CheckTestStatusTest.java
@@ -13,8 +13,13 @@ import org.testng.annotations.Test;
 
 public class CheckTestStatusTest extends TesterraTest {
 
-    private final Log4jFileReader log4jFileReader = new Log4jFileReader("logs/pre-test-log4j.log");
-    private final String methodEndWorkerName = "finish.MethodEndWorker";
+    // log file reader
+    private final Log4jFileReader LOG_4_J_FILE_READER = new Log4jFileReader("logs/pre-test-log4j.log");
+
+    // terms for searching log file
+    private final String METHOD_START_WORKER_SEARCH_TERM = "start.MethodStartWorker";
+    private final String METHOD_END_WORKER_SEARCH_TERM = "finish.MethodEndWorker";
+    private final String RETRY_ANALYZER_SEARCH_TERM = "testng.RetryAnalyzer";
 
     // only test method names that have one specific Status shown in entry with the method name
     @DataProvider
@@ -22,6 +27,7 @@ public class CheckTestStatusTest extends TesterraTest {
         return new Object[][]{
                 {"test_Failed", Status.FAILED },
                 {"test_Passed", Status.PASSED },
+                {"testAssertCollector", Status.FAILED},
                 {"test_expectedFailed", Status.FAILED_EXPECTED},
                 {"test_expectedFailedPassed", Status.PASSED},
                 {"test_validExpectedFailed_withMethod", Status.FAILED_EXPECTED},
@@ -34,8 +40,9 @@ public class CheckTestStatusTest extends TesterraTest {
     @DataProvider
     public static Object[][] provideTestMethodsSkipped() {
         return new Object[][]{
-                {"test_Skipped"},
-                {"test_SkippedNoStatus"},
+                {"test_Skipped", Status.FAILED, Status.SKIPPED.title},
+                {"test_SkippedNoStatus", Status.FAILED, Status.SKIPPED.title},
+                {"test_Skipped_dependingOnFailed", Status.NO_RUN, "depends on not successfully finished methods"},
         };
     }
 
@@ -52,80 +59,112 @@ public class CheckTestStatusTest extends TesterraTest {
     @DataProvider
     public static Object[][] provideTestMethodsRetried() {
         return new Object[][]{
-                {"testCaseOne"},
-                {"test_retryOnWebDriverException"},
-                {"test_retryOnUnreachableBrowserException"},
-                {"test_retryOnJsonException"}
+                {"testCaseOne", Status.RECOVERED},
+                {"test_retryOnWebDriverException", Status.RECOVERED},
+                {"test_retryOnUnreachableBrowserException", Status.RECOVERED},
+                {"test_retryOnJsonException", Status.RECOVERED},
+                {"test_RetryFailed", Status.FAILED},
+                {"test_RetryExpectedFailed", Status.FAILED_EXPECTED},
+                {"test_RetryValidExpectedFailed", Status.FAILED_EXPECTED},
+                {"test_RetryInvalidExpectedFailed", Status.FAILED}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] provideTestMethodsNoRetry() {
+        return new Object[][]{
+                {"test_NoRetry"}
         };
     }
 
     @BeforeClass
     public void verifyLogFileExists() {
-        Assert.assertTrue(log4jFileReader.existsFile(), "log file exists.");
+        Assert.assertTrue(LOG_4_J_FILE_READER.existsFile(), "log file exists.");
     }
 
     @Test(dataProvider = "provideExpectedStatusPerMethod")
     public void verifySimpleTestStatus(final String methodName, final Status expectedTestStatus) {
-        log4jFileReader.assertTestStatusPerMethod(methodEndWorkerName, methodName, expectedTestStatus);
+        LOG_4_J_FILE_READER.assertTestStatusPerMethod(METHOD_END_WORKER_SEARCH_TERM, methodName, expectedTestStatus);
     }
 
     @Test(dataProvider = "providePriorityPerMethod")
     public void verifyPriorityStatus(final String methodName, final Status expectedTestStatus, final int expectedPriority) {
-
-        final String methodStartWorkerName = "start.MethodStartWorker";
-
         // check basic test status
-        log4jFileReader.assertTestStatusPerMethod(methodEndWorkerName, methodName, expectedTestStatus);
+        LOG_4_J_FILE_READER.assertTestStatusPerMethod(METHOD_END_WORKER_SEARCH_TERM, methodName, expectedTestStatus);
 
         // check priority information
-        final List<String> methodEndEntries = log4jFileReader.filterLogForTestMethod(methodStartWorkerName, methodName);
-        Assert.assertEquals(methodEndEntries.size(), 1,
+        final List<String> methodStartEntries = LOG_4_J_FILE_READER.filterLogForTestMethod(METHOD_START_WORKER_SEARCH_TERM, methodName);
+        Assert.assertEquals(methodStartEntries.size(), 1,
                 "correct amount of method start entries found.");
 
-        final String priorityEntry = log4jFileReader.filterLogForFollowingEntry(methodEndEntries.get(0));
+        final String priorityEntry = LOG_4_J_FILE_READER.filterLogForFollowingEntry(methodStartEntries.get(0));
         Assert.assertTrue(priorityEntry.contains(String.valueOf(expectedPriority)),
                 String.format("priority '%s' found in entry '%s'", expectedPriority, priorityEntry));
     }
 
     @Test(dataProvider = "provideTestMethodsRetried")
-    public void verifyRetryStatus(final String methodName) {
+    public void verifyRetryStatus(final String methodName, final Status finalStatus) {
 
-        final String classNameForRetrySlug = "testng.RetryAnalyzer";
+
         final String failedStatus = Status.FAILED.title;
-        final String recoveredStatus = Status.RECOVERED.title;
+        final String finalStatusTitle = finalStatus.title;
 
         // check basic test status in log
-        final List<String> methodEndEntries = log4jFileReader.filterLogForTestMethod(methodEndWorkerName, methodName);
+        final List<String> methodEndEntries = LOG_4_J_FILE_READER.filterLogForTestMethod(METHOD_END_WORKER_SEARCH_TERM, methodName);
 
         Assert.assertEquals(methodEndEntries.size(), 2,
                 "correct amount of test status entries found.");
         Assert.assertTrue(methodEndEntries.get(0).contains(failedStatus),
                 String.format("'%s' has status '%s'", methodName, failedStatus));
-        Assert.assertTrue(methodEndEntries.get(1).contains(recoveredStatus),
-                String.format("'%s' has status '%s'", methodName, recoveredStatus));
+        Assert.assertTrue(methodEndEntries.get(1).contains(finalStatusTitle),
+                String.format("'%s' has status '%s'", methodName, finalStatusTitle));
 
-        // check retry information
-        final List<String> retryEntries = log4jFileReader.filterLogForTestMethod(classNameForRetrySlug, methodName);
-
-        Assert.assertEquals(retryEntries.size(), 1,
-                String.format("'%s' was retried '%s' time", methodName, 1));
+        // check retry information, check of actual status 'retried' not possible in log
+        final List<String> retryEntries = LOG_4_J_FILE_READER.filterLogForTestMethod(RETRY_ANALYZER_SEARCH_TERM, methodName);
 
         final String onlyRetryEntry = retryEntries.get(0);
-        Assert.assertTrue(onlyRetryEntry.contains("1/1"),
+        Assert.assertTrue(onlyRetryEntry.contains("Send signal for retrying the test (1/1)"),
                 String.format("entry '%s' has information on initiation of one retry", onlyRetryEntry));
+
+        // run limit information only logged for failed tests
+        if (finalStatus.equals(Status.FAILED) || finalStatus.equals(Status.FAILED_EXPECTED)) {
+            final String retryLimitEntry = retryEntries.get(1);
+            Assert.assertTrue(retryLimitEntry.contains("limit (1)"),
+                    String.format("entry '%s' has information on abortion of retry due to exceeded limit", retryLimitEntry));
+        } else {
+            Assert.assertEquals(retryEntries.size(), 1,
+                    String.format("'%s' was retried '%s' time", methodName, 1));
+        }
+    }
+
+    @Test(dataProvider = "provideTestMethodsNoRetry")
+    public void verifyNoRetryStatus(final String methodName) {
+
+        final String failedStatus = Status.FAILED.title;
+
+        final List<String> methodEndEntries = LOG_4_J_FILE_READER.filterLogForTestMethod(METHOD_END_WORKER_SEARCH_TERM, methodName);
+        final String logLine = methodEndEntries.get(0);
+
+        Assert.assertEquals(methodEndEntries.size(), 1,
+                "correct amount of test status entries found.");
+        Assert.assertTrue(logLine.contains(failedStatus),
+                String.format("'%s' has status '%s' in log line '%s'", methodName, failedStatus, logLine));
+
+        final List<String> retryEntries = LOG_4_J_FILE_READER.filterLogForTestMethod(RETRY_ANALYZER_SEARCH_TERM, methodName);
+        Assert.assertEquals(retryEntries.size(), 0,
+                String.format("no information on initiation of retry found for test '%s'", methodName));
     }
 
     @Test
     public void testExpectedFailsStatus() {
 
         final String methodName = "test_retriedExpectedFailed";
-        final String classNameForRetrySlug = "testng.RetryAnalyzer";
 
         // check basic test status
-        log4jFileReader.assertTestStatusPerMethod(methodEndWorkerName, methodName, Status.FAILED_EXPECTED);
+        LOG_4_J_FILE_READER.assertTestStatusPerMethod(METHOD_END_WORKER_SEARCH_TERM, methodName, Status.FAILED_EXPECTED);
 
         // check retry information
-        final List<String> retryEntries = log4jFileReader.filterLogForTestMethod(classNameForRetrySlug, methodName);
+        final List<String> retryEntries = LOG_4_J_FILE_READER.filterLogForTestMethod(RETRY_ANALYZER_SEARCH_TERM, methodName);
 
         Assert.assertEquals(retryEntries.size(), 3,
                 String.format("'%s' was retried '%s' time", methodName, 3));
@@ -134,29 +173,31 @@ public class CheckTestStatusTest extends TesterraTest {
         final String secondRetryEntry = retryEntries.get(1);
         final String retryLimitEntry = retryEntries.get(2);
 
-        Assert.assertTrue(firstRetryEntry.contains("1/2"),
+        Assert.assertTrue(firstRetryEntry.contains("Send signal for retrying the test (1/2)"),
                 String.format("entry '%s' has information on initiation of first retry", firstRetryEntry));
-        Assert.assertTrue(secondRetryEntry.contains("2/2"),
+        Assert.assertTrue(secondRetryEntry.contains("Send signal for retrying the test (2/2)"),
                 String.format("entry '%s' has information on initiation of second retry", secondRetryEntry));
         Assert.assertTrue(retryLimitEntry.contains("run limit (2)"),
                 String.format("entry '%s' has information on maximum retries", retryLimitEntry));
     }
 
     @Test(dataProvider = "provideTestMethodsSkipped")
-    public void testSkippedStatus(final String methodName) {
-        final String failedStatus = Status.FAILED.title;
-        final String skippedStatus = Status.SKIPPED.title.toLowerCase();
+    public void testSkippedStatus(final String methodName, final Status expectedInitialStatus, final String skippedInformationInLog) {
+
+        final String expectedInitialStatusTitle = expectedInitialStatus.title;
 
         // check basic test status in log
-        final List<String> foundEntries = log4jFileReader.filterLogForTestMethod(methodEndWorkerName, methodName);
+        final List<String> foundEntries = LOG_4_J_FILE_READER.filterLogForTestMethod(METHOD_END_WORKER_SEARCH_TERM, methodName);
+        final String logLine = foundEntries.get(0);
+
         Assert.assertEquals(foundEntries.size(), 1,
                 "correct amount of method end entries found.");
-        Assert.assertTrue(foundEntries.get(0).contains(failedStatus),
-                String.format("'%s' has status '%s'", methodName, failedStatus));
+        Assert.assertTrue(logLine.contains(expectedInitialStatusTitle),
+                String.format("'%s' has status '%s' in log line '%s'", methodName, expectedInitialStatusTitle, logLine));
 
         // check entry with information for skipped
-        final String skippedEntry = log4jFileReader.filterLogForFollowingEntry(foundEntries.get(0));
-        Assert.assertTrue(skippedEntry.contains(skippedStatus),
-                String.format("status '%s' found in entry '%s'", skippedStatus, skippedEntry));
+        final String skippedMethodEntry = LOG_4_J_FILE_READER.filterLogForFollowingEntry(logLine);
+        Assert.assertTrue(skippedMethodEntry.contains(skippedInformationInLog),
+                String.format("status '%s' found in entry '%s'", skippedInformationInLog, skippedMethodEntry));
     }
 }

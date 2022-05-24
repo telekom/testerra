ee3726edeafc1c42bffa92bd21f913899981e3cf
Clemens Grosse <Clemens.Grosse@t-systems.com>
2022-05-24 15:42:02 +0200

    added usage of Status in ReportFailureAspectsPage

--- report-ng-tests/src/main/java/io/testerra/report/test/pages/report/ReportFailureAspectsPage.java
+++ report-ng-tests/src/main/java/io/testerra/report/test/pages/report/ReportFailureAspectsPage.java
@@ -2,14 +2,15 @@ package io.testerra.report.test.pages.report;
 
 import eu.tsystems.mms.tic.testframework.pageobjects.Check;
 import eu.tsystems.mms.tic.testframework.pageobjects.GuiElement;
-import io.testerra.report.test.pages.AbstractReportPage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
+import eu.tsystems.mms.tic.testframework.report.Status;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import io.testerra.report.test.pages.AbstractReportPage;
 
 public class ReportFailureAspectsPage extends AbstractReportPage {
 
@@ -31,7 +32,7 @@ public class ReportFailureAspectsPage extends AbstractReportPage {
         for (GuiElement row : tableRows) {
             statusContainsFailed = false;
             for (GuiElement status : row.getSubElement(By.xpath("(//td)[3]/div")).getList()) {
-                if (status.getText().contains("Failed")) {
+                if (status.getText().contains(Status.FAILED.title)) {
                     if (status.getText().split(" ").length > 2)
                         continue;    //skips expected fails
                     statusContainsFailed = true;

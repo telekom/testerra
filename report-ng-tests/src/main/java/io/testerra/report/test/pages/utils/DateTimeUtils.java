eeaafd641f4bc6b9601cc745888ce1bbae645131
Clemens Grosse <Clemens.Grosse@t-systems.com>
2022-05-24 15:36:15 +0200

    added DateTimeUtils and RegExUtils; refactored ReportDashBordPage, ReportFailureAspectsPage and ReportSideBar; updated ReportDashBoardPageTest;

---
+++ report-ng-tests/src/main/java/io/testerra/report/test/pages/utils/DateTimeUtils.java
@@ -0,0 +1,36 @@
+package io.testerra.report.test.pages.utils;
+
+import java.text.SimpleDateFormat;
+
+public class DateTimeUtils {
+
+    public static final SimpleDateFormat formatSeconds = new SimpleDateFormat("s's' SSS'ms'");
+    public static final SimpleDateFormat formatMinutes = new SimpleDateFormat("mm'mins' s's' SSS'ms'");
+    public static final SimpleDateFormat formatHours = new SimpleDateFormat("H'h' mm'mins' s's' SSS'ms'");
+
+
+    public static boolean verifyDateTimeString(final String dateTimeString) {
+       return verifyFormat(dateTimeString);
+    }
+
+    private static boolean verifyFormat(final String dateTimeString) {
+        if (isParsable(dateTimeString, DateTimeUtils.formatSeconds)) {
+            return true;
+        }
+
+        if (isParsable(dateTimeString, DateTimeUtils.formatMinutes)) {
+            return true;
+        }
+
+        return isParsable(dateTimeString, DateTimeUtils.formatHours);
+    }
+
+    public static boolean isParsable(final String dateTimeString, final SimpleDateFormat format) {
+        try {
+            format.parse(dateTimeString);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ report-ng-tests/src/main/java/io/testerra/report/test/pages/utils/RegExUtils.java
@@ -0,0 +1,44 @@
+package io.testerra.report.test.pages.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegExUtils {
+
+    public static Matcher createMatcherForRegExp(final RegExp standardRegExp, final String evaluationString) {
+
+        Pattern pattern = Pattern.compile(standardRegExp.getPatternString());
+        return pattern.matcher(evaluationString);
+    }
+
+    public static String getRegExpResultOfString(final RegExp standardRegExp, String line) {
+
+        final Matcher matcher = createMatcherForRegExp(standardRegExp, line);
+
+        if (matcher.find()) {
+            return matcher.group();
+        }
+
+        return "";
+    }
+
+
+    public enum RegExp {
+        DIGITS_ONLY("\\d+");
+
+        private String patternString;
+
+        RegExp(final String patternString) {
+            this.patternString = patternString;
+        }
+
+        public String getPatternString() {
+            return this.patternString;
+        }
+
+        public void setPatternString(final String valueToSet) {
+            this.patternString=valueToSet;
+        }
+
+    }
+}

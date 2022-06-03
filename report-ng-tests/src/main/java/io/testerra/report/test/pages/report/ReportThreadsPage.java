1dbd5fc01e1f3e8df1152dcbf54eaa7b6404d603
marc2202 <Marc.Dietrich@t-systems.com>
2022-06-03 12:15:18 +0200

    - added test for ReportThreadsPage

--- report-ng-tests/src/main/java/io/testerra/report/test/pages/report/ReportThreadsPage.java
+++ report-ng-tests/src/main/java/io/testerra/report/test/pages/report/ReportThreadsPage.java
@@ -1,9 +1,26 @@
 package io.testerra.report.test.pages.report;
 
+import eu.tsystems.mms.tic.testframework.pageobjects.Check;
+import eu.tsystems.mms.tic.testframework.pageobjects.GuiElement;
+import eu.tsystems.mms.tic.testframework.utils.TimerUtils;
 import io.testerra.report.test.pages.AbstractReportPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ReportThreadsPage extends AbstractReportPage {
+
+    @Check
+    private final GuiElement testMethodSearchbar = pageContent.getSubElement(By.xpath("//label[@label='Method']//input"));
+    @Check
+    private final GuiElement testMethodDropDownList = pageContent.getSubElement(By.xpath("//div[./label[@label='Method']]//mdc-lookup"));
+    @Check
+    private final GuiElement testThreadReport = pageContent.getSubElement(By.xpath("//div[@class='vis-foreground']/div"));
+
     /**
      * Constructor for existing sessions.
      *
@@ -11,5 +28,45 @@ public class ReportThreadsPage extends AbstractReportPage {
      */
     public ReportThreadsPage(WebDriver driver) {
         super(driver);
+    }
+
+    public Set<String> getThreadMethods() {
+        testMethodSearchbar.click();
+        Set<String> set = testMethodDropDownList.getSubElement(By.xpath("//mdc-list-item")).getList()
+                .stream()
+                .map(GuiElement::getText)
+                .collect(Collectors.toSet());
+        Assert.assertTrue(set.size() > 0, "Should find some methods!");
+        testMethodSearchbar.click();
+        return set;
+    }
+
+    public void assertSearchForMethodWorksCorrect(Set<String> methods) {
+        for (String method : methods) {
+            selectMethod(method);
+            assertMethodBoxIsSelected(method);
+        }
+    }
+
+    private void selectMethod(String method) {
+        testMethodSearchbar.click();
+        testMethodSearchbar.type(method);
+        TimerUtils.sleep(1000, "Necessary sleep gives page enough time to refresh target locators");
+        Optional<GuiElement> optional = testMethodDropDownList.getSubElement(By.xpath("//mdc-list-item")).getList()
+                .stream()
+                .filter(i -> i.getText().contains(method))
+                .findFirst();
+        Assert.assertTrue(optional.isPresent());
+        optional.get().click();
+    }
+
+    private void assertMethodBoxIsSelected(String method) {
+        for (GuiElement guiElement : testThreadReport.getList()) {
+            if (guiElement.getText().split("\n")[0].equals(method.trim())) {
+                guiElement.getSubElement(By.xpath("/div"))
+                        .asserts("Searched element should marked as selected")
+                        .assertAttributeContains("class", "vis-selected");
+            }
+        }
     }
 }
---
+++ report-ng-tests/src/test/java/io/testerra/report/test/ReportThreadsPageTest.java
@@ -0,0 +1,31 @@
+package io.testerra.report.test;
+
+import eu.tsystems.mms.tic.testframework.common.PropertyManager;
+import eu.tsystems.mms.tic.testframework.report.model.steps.TestStep;
+import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
+import io.testerra.report.test.pages.ReportPageType;
+import io.testerra.report.test.pages.report.ReportDashBoardPage;
+import io.testerra.report.test.pages.report.ReportThreadsPage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class ReportThreadsPageTest extends AbstractReportTest {
+
+    @Test
+    public void testT01_checkSearchForMethodsSelectionWorksCorrectly() {
+        WebDriver driver = WebDriverManager.getWebDriver();
+
+        TestStep.begin("Navigate to dashboard page.");
+        ReportDashBoardPage reportDashBoardPage = this.visitTestPage(ReportDashBoardPage.class, driver, PropertyManager.getProperty("file.path.content.root"));
+
+        TestStep.begin("Navigate to threads page.");
+        ReportThreadsPage reportThreadsPage = reportDashBoardPage.gotoToReportPage(ReportPageType.THREADS, ReportThreadsPage.class);
+
+        TestStep.begin("Check whether thread report contains all methods");
+        Set<String> methods = reportThreadsPage.getThreadMethods();
+        reportThreadsPage.assertSearchForMethodWorksCorrect(methods);
+    }
+
+}

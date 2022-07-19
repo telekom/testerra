ee48b4a8f490a898f2d0b8aeff5c3d53efea0ac1
Martin Großmann <martin.grossmann@t-systems.com>
2022-07-19 18:36:38 +0200

    Added some playground test classes for it.

---
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/playground/ExclusiveSession1.java
@@ -0,0 +1,40 @@
+package eu.tsystems.mms.tic.testframework.playground;
+
+import eu.tsystems.mms.tic.testframework.AbstractTestSitesTest;
+import eu.tsystems.mms.tic.testframework.testing.PageFactoryProvider;
+import eu.tsystems.mms.tic.testframework.utils.TimerUtils;
+import eu.tsystems.mms.tic.testframework.utils.UITestUtils;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created on 19.07.2022
+ *
+ * @author mgn
+ */
+public class ExclusiveSession1 extends AbstractTestSitesTest implements PageFactoryProvider {
+
+    private String exclusiveSessionId = "";
+
+    @BeforeClass
+    public void beforeClass() {
+        WebDriver driver = getWebDriver();
+        exclusiveSessionId = WEB_DRIVER_MANAGER.makeExclusive(driver);
+    }
+
+    @Test
+    public void testT01SessionTest() {
+        WEB_DRIVER_MANAGER.getWebDriver(exclusiveSessionId);
+        TimerUtils.sleep(2_000, "Extend test duration");
+        UITestUtils.takeScreenshots();
+        TimerUtils.sleep(5_000, "Extend test duration");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        WEB_DRIVER_MANAGER.shutdownSession(exclusiveSessionId);
+    }
+
+}
---
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/playground/ExclusiveSession2.java
@@ -0,0 +1,42 @@
+package eu.tsystems.mms.tic.testframework.playground;
+
+import eu.tsystems.mms.tic.testframework.AbstractTestSitesTest;
+import eu.tsystems.mms.tic.testframework.core.pageobjects.testdata.PageWithNotExistingElement;
+import eu.tsystems.mms.tic.testframework.core.testpage.TestPage;
+import eu.tsystems.mms.tic.testframework.testing.PageFactoryProvider;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created on 19.07.2022
+ *
+ * @author mgn
+ */
+public class ExclusiveSession2 extends AbstractTestSitesTest implements PageFactoryProvider {
+
+    private String exclusiveSessionId = "";
+
+    protected TestPage getTestPage() {
+        return TestPage.LAYOUT;
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        WebDriver driver = getWebDriver();
+        exclusiveSessionId = WEB_DRIVER_MANAGER.makeExclusive(driver);
+    }
+
+    @Test
+    public void testT02SessionTest() {
+        WebDriver driver = WEB_DRIVER_MANAGER.getWebDriver(exclusiveSessionId);
+        PAGE_FACTORY.createPage(PageWithNotExistingElement.class, driver);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        WEB_DRIVER_MANAGER.shutdownSession(exclusiveSessionId);
+    }
+
+}

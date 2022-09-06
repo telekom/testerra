49e008b2c953d2e727c2f5ea3526c63acea3d082
marc2202 <Marc.Dietrich@t-systems.com>
2022-09-06 13:57:19 +0200

    - adjusted copyrights: contributor

--- report-ng-tests/src/main/java/io/testerra/report/test/pages/utils/DateTimeUtils.java
+++ report-ng-tests/src/main/java/io/testerra/report/test/pages/utils/DateTimeUtils.java
@@ -1,7 +1,7 @@
 /*
  * Testerra
  *
- * (C) 2020, Eric Kubenka, T-Systems Multimedia Solutions GmbH, Deutsche Telekom AG
+ * (C) 2022, Clemens Große, T-Systems Multimedia Solutions GmbH, Deutsche Telekom AG
  *
  * Deutsche Telekom AG and all other contributors /
  * copyright owners license this file to you under the Apache

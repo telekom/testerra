68a69ece1a9ba3c425a4f8e93b972a4ffb2fb063
Martin Großmann <martin.grossmann@t-systems.com>
2022-10-05 10:37:59 +0200

    Optimized imports

--- core/src/main/java/eu/tsystems/mms/tic/testframework/utils/SourceUtils.java
+++ core/src/main/java/eu/tsystems/mms/tic/testframework/utils/SourceUtils.java
@@ -33,8 +33,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;

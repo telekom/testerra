ee3645a060a1d6b8b8c71f9f65f55d47e28cf0ff
Martin Großmann <martin.grossmann@t-systems.com>
2023-04-21 09:06:50 +0200

    Updated extensions test for chrome

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/test/webdrivermanager/DesktopWebDriverFactoryTest.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/test/webdrivermanager/DesktopWebDriverFactoryTest.java
@@ -21,21 +21,31 @@
 
 package eu.tsystems.mms.tic.testframework.test.webdrivermanager;
 
+import eu.tsystems.mms.tic.testframework.constants.Browsers;
+import eu.tsystems.mms.tic.testframework.pageobjects.UiElement;
+import eu.tsystems.mms.tic.testframework.pageobjects.UiElementFinder;
 import eu.tsystems.mms.tic.testframework.report.model.context.SessionContext;
 import eu.tsystems.mms.tic.testframework.testing.TesterraTest;
+import eu.tsystems.mms.tic.testframework.testing.UiElementFinderFactoryProvider;
 import eu.tsystems.mms.tic.testframework.testing.WebDriverManagerProvider;
+import eu.tsystems.mms.tic.testframework.useragents.ChromeConfig;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.DesktopWebDriverRequest;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverRequest;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.util.Map;
+import java.util.Objects;
 
-public class DesktopWebDriverFactoryTest extends TesterraTest implements WebDriverManagerProvider {
+public class DesktopWebDriverFactoryTest extends TesterraTest implements WebDriverManagerProvider, UiElementFinderFactoryProvider {
 
     @Test
     public void testT01_BaseURL() throws Exception {
@@ -92,6 +102,32 @@ public class DesktopWebDriverFactoryTest extends TesterraTest implements WebDriv
         WebDriverRequest webDriverRequest = WEB_DRIVER_MANAGER.getSessionContext(driver).get().getWebDriverRequest();
 
         Assert.assertEquals(webDriverRequest.getCapabilities().get(CapabilityType.PLATFORM_NAME), Platform.LINUX);
+    }
+
+    @Test
+    public void testT05_ChromeExtensions() throws MalformedURLException {
+
+        File chromeExtensionFile = new File(
+                Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("testfiles/Simple_Translate_2.8.1.0.crx")).getFile());
+
+        WEB_DRIVER_MANAGER.setUserAgentConfig(Browsers.chrome, (ChromeConfig) options -> {
+            options.setAcceptInsecureCerts(true);
+        });
+
+
+        ChromeOptions options = new ChromeOptions();
+        options.addExtensions(chromeExtensionFile);
+        DesktopWebDriverRequest request = new DesktopWebDriverRequest();
+//        request.setBaseUrl("chrome://extensions-internals/");
+        request.getDesiredCapabilities().merge(options);
+
+        WebDriver webDriver = WEB_DRIVER_MANAGER.getWebDriver(request);
+        webDriver.get("chrome://extensions-internals/");
+        UiElementFinder uiElementFinder = UI_ELEMENT_FINDER_FACTORY.create(webDriver);
+        UiElement chromeExtensionJson = uiElementFinder.find(By.xpath("//pre"));
+        String content = chromeExtensionJson.waitFor().text().getActual();
+        Assert.assertTrue(content.contains("Simple Translate"));
+
     }
 
 }

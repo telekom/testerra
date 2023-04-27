3050a99fd3da68a6b198687f4e51aa8e3235f442
Martin Großmann <martin.grossmann@t-systems.com>
2023-04-27 17:44:05 +0200

    Improved tests for window switch

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/test/webdrivermanager/WebDriverManagerSwitchWindowTests.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/test/webdrivermanager/WebDriverManagerSwitchWindowTests.java
@@ -42,7 +42,8 @@ public class WebDriverManagerSwitchWindowTests extends AbstractTestSitesTest imp
         UiElement header2 = getFinder(webDriver).find(By.xpath("//h1[text() = 'Window 2']"));
 
         link1.click();
-        WEB_DRIVER_MANAGER.switchToWindow(driver -> driver.getCurrentUrl().contains("window2.html"));
+        boolean result = WEB_DRIVER_MANAGER.switchToWindow(driver -> driver.getCurrentUrl().contains("window2.html"));
+        ASSERT.assertTrue(result);
         header2.assertThat().present(true);
     }
 
@@ -71,6 +72,35 @@ public class WebDriverManagerSwitchWindowTests extends AbstractTestSitesTest imp
         ASSERT.assertEquals(webDriver.getWindowHandles().size(), 3);
     }
 
+    @Test
+    public void testT05_DelayedNewWindow() {
+        WebDriver webDriver = this.getWebDriver();
+        UiElement link3 = getFinder(webDriver).find(By.id("link3"));
+        UiElement header2 = getFinder(webDriver).find(By.xpath("//h1[text() = 'Window 2']"));
+        final String handle1 = webDriver.getWindowHandle();
+        link3.click();
+
+        CONTROL.waitFor(6, () -> WEB_DRIVER_MANAGER.switchToWindowTitle(webDriver, "Window 2"));
+        header2.assertThat().present(true);
+        ASSERT.assertNotEquals(webDriver.getWindowHandle(), handle1);
+    }
+
+    @Test
+    public void testT06_DelayedNewWindowWithPredicate() {
+        WebDriver webDriver = this.getWebDriver();
+        UiElement link3 = getFinder(webDriver).find(By.id("link3"));
+        UiElement header2 = getFinder(webDriver).find(By.xpath("//h1[text() = 'Window 2']"));
+        final String handle1 = webDriver.getWindowHandle();
+        link3.click();
+
+        CONTROL.waitFor(6, () -> {
+            boolean result = WEB_DRIVER_MANAGER.switchToWindow(webDriver, driver -> driver.getTitle().contains("Window 2"));
+            ASSERT.assertTrue(result);
+        });
+        header2.assertThat().present(true);
+        ASSERT.assertNotEquals(webDriver.getWindowHandle(), handle1);
+    }
+
     @Test(
             expectedExceptions = UiElementAssertionError.class,
             expectedExceptionsMessageRegExp = "Expected that UiElement\\(By.xpath: //h1\\[text\\(\\) = 'Window 2'\\]\\) present is true"
@@ -82,6 +112,32 @@ public class WebDriverManagerSwitchWindowTests extends AbstractTestSitesTest imp
 
         link1.click();
         header2.assertThat().present(true);
+    }
+
+    @Test(
+            expectedExceptions = RuntimeException.class,
+            expectedExceptionsMessageRegExp = "Window title \"Wrong title\" not found"
+    )
+    public void testT11_SwitchToWindowWithWrongTitle() {
+        WebDriver webDriver = this.getWebDriver();
+        UiElement link1 = getFinder(webDriver).find(By.id("link1"));
+
+        link1.click();
+        WEB_DRIVER_MANAGER.switchToWindowTitle("Wrong title");
+    }
+
+    @Test
+    public void testT12_SwitchToWindowWithWrongTitlePredicate() {
+        WebDriver webDriver = this.getWebDriver();
+        UiElement link1 = getFinder(webDriver).find(By.id("link1"));
+        UiElement header1 = getFinder(webDriver).find(By.xpath("//h1[text() = 'Window 1']"));
+        final String handle1 = webDriver.getWindowHandle();
+
+        link1.click();
+        boolean result = WEB_DRIVER_MANAGER.switchToWindow(webDriver, driver -> driver.getTitle().contains("Wrong title"));
+        ASSERT.assertFalse(result);
+        header1.assertThat().present(true);
+        ASSERT.assertEquals(webDriver.getWindowHandle(), handle1);
     }
 
     private UiElementFinder getFinder(WebDriver driver) {

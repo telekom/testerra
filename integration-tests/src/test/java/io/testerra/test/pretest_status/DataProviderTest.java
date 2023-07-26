2dc4704a1004d6cbe4aeeac74bd494312ba526d0
Martin Großmann <github@martymaximus.de>
2023-07-26 10:17:37 +0200

    Added retry test in dataprovider

--- integration-tests/src/test/java/io/testerra/test/pretest_status/DataProviderTest.java
+++ integration-tests/src/test/java/io/testerra/test/pretest_status/DataProviderTest.java
@@ -27,6 +27,8 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class DataProviderTest extends TesterraTest implements AssertProvider {
 
     /**
@@ -104,6 +106,19 @@ public class DataProviderTest extends TesterraTest implements AssertProvider {
      */
     @Test(dataProvider = "it-does-not-exist")
     public void testT07_NonExistingDataProvider() {
+    }
+
+    private AtomicInteger counter = new AtomicInteger(0);
+
+//    @Test(dataProvider = "dataProviderSimple")
+    public void testT08_DataProviderWithRetry(String dp) {
+        if ("failed".equals(dp)) {
+            this.counter.incrementAndGet();
+            if (counter.get() == 1) {
+                // Retry message is defined in test.properties
+                Assert.fail("test_T07_RetriedDataProvider");
+            }
+        }
     }
 
 }

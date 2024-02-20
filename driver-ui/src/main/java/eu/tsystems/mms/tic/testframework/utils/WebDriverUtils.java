342e479753bd59f2ea95a1df633469cf40e9bbb5
Martin Großmann <github@martymaximus.de>
2024-02-20 13:13:03 +0100

    Cleanup

--- driver-ui/src/main/java/eu/tsystems/mms/tic/testframework/utils/WebDriverUtils.java
+++ driver-ui/src/main/java/eu/tsystems/mms/tic/testframework/utils/WebDriverUtils.java
@@ -28,13 +28,11 @@ import eu.tsystems.mms.tic.testframework.exceptions.SystemException;
 import eu.tsystems.mms.tic.testframework.transfer.ThrowablePackedResponse;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.IWebDriverManager;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
-import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverSessionsManager;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -239,38 +237,6 @@ public final class WebDriverUtils {
     static void deleteAllCookies(final WebDriver driver) {
         driver.manage().deleteAllCookies();
     }
-
-//    /**
-//     * Will search for the deepest {@link WebDriver} in the Wrapped-WebDriver-Chain
-//     *
-//     * @param driver {@link WebDriver}
-//     * @return WebDriver
-//     */
-//    public static WebDriver getLowestWebDriver(WebDriver driver) {
-//        if (driver instanceof Decorated) {
-//            return ((Decorated<WebDriver>) driver).getOriginal();
-//        }
-//        LOGGER.warn("You already have the lowest WebDriver, but it's decorated.");
-//        return driver;
-//    }
-
-//    /**
-//     * Will return Selenium session UUID of {@link WebDriver} session
-//     *
-//     * @param driver {@link WebDriver}
-//     * @return String
-//     * @deprecated Use {@link WebDriverSessionsManager#getSessionContext(WebDriver)} instead
-//     * // TODO Should be package private
-//     */
-//    public static String getSessionId(WebDriver driver) {
-//        driver = getLowestWebDriver(driver);
-//        if (driver instanceof RemoteWebDriver) {
-//            RemoteWebDriver remoteWebDriver = (RemoteWebDriver) driver;
-//            return remoteWebDriver.getSessionId().toString();
-//        }
-//
-//        return null;
-//    }
 
     /**
      * @deprecated Use {@link JSUtils#getViewport(WebDriver)} instead

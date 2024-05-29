156e0447744a069c0a84a709a599d91f66e95bb6
Martin Großmann <github@martymaximus.de>
2024-05-29 11:46:58 +0200

    Updated playground test

--- integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/playground/DriverStandaloneTest.java
+++ integration-tests/src/test/java/eu/tsystems/mms/tic/testframework/playground/DriverStandaloneTest.java
@@ -3,8 +3,14 @@ package eu.tsystems.mms.tic.testframework.playground;
 import eu.tsystems.mms.tic.testframework.AbstractWebDriverTest;
 import eu.tsystems.mms.tic.testframework.constants.Browsers;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.DesktopWebDriverRequest;
-import org.testng.Assert;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.attribute.PosixFileAttributeView;
 
 /**
  * Created on 2024-05-29
@@ -19,8 +25,13 @@ public class DriverStandaloneTest extends AbstractWebDriverTest {
         request.setBaseUrl("http://google.de");
         request.setBrowser(Browsers.chrome);
 
-        WEB_DRIVER_MANAGER.getWebDriver(request);
-        Assert.assertTrue(false);
+        WebDriver webDriver = WEB_DRIVER_MANAGER.getWebDriver(request);
+//        UITestUtils.takeScreenshot(webDriver, false);
+//        Assert.assertTrue(false);
+        File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+        PosixFileAttributeView fileAttributeView = Files.getFileAttributeView(file.toPath(), PosixFileAttributeView.class);
+        log().info("take screenshot src {}", file.toPath());
+        log().info("Posix {}", fileAttributeView.toString());
     }
 
 }

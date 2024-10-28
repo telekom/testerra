35e192588ff19daafa5b50b0be63228a43114922
sena <selina.natschke@t-systems.com>
2024-10-28 10:43:50 +0100

    added " to searchable strings

--- report-ng/app/src/services/status-converter.ts
+++ report-ng/app/src/services/status-converter.ts
@@ -183,7 +183,7 @@ export class StatusConverter {
     }
 
     createRegexpFromSearchString(searchQuery:string) {
-        const specialCharacters = new RegExp("([.*+?^${}()|[\\]\\\\])", "g");
+        const specialCharacters = new RegExp('([.*+?^${}()|[\\]\\\\"])', "g");
         searchQuery = searchQuery.replace(specialCharacters, "\\$1");
         return new RegExp("(" + searchQuery + ")", "ig");
     }

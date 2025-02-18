b10fda9e5461e249fb24244e5f94613a47b604fc
Tobias Adler <Tobias.Adler@telekom.de>
2025-02-18 10:16:36 +0100

    Added pretests for history

---
+++ integration-tests/src/test/java/io/testerra/test/pretest_history/DynamicHistoryTest.java
@@ -0,0 +1,68 @@
+package io.testerra.test.pretest_history;
+
+import eu.tsystems.mms.tic.testframework.testing.AssertProvider;
+import eu.tsystems.mms.tic.testframework.testing.TesterraTest;
+import eu.tsystems.mms.tic.testframework.utils.TimerUtils;
+import org.testng.annotations.Test;
+
+import static eu.tsystems.mms.tic.testframework.common.PropertyManagerProvider.PROPERTY_MANAGER;
+
+public class DynamicHistoryTest extends TesterraTest implements AssertProvider {
+
+    @Test
+    public void test_highFlakiness() {
+        int historyIndex = Integer.parseInt(PROPERTY_MANAGER.getProperty("historyIndex"));
+        boolean x = (historyIndex % 2 != 0);
+        ASSERT.assertTrue(x);
+    }
+
+    @Test
+    public void test_passedToFailed() {
+        int historyIndex = Integer.parseInt(PROPERTY_MANAGER.getProperty("historyIndex"));
+        ASSERT.assertTrue(historyIndex < 5);
+    }
+
+    @Test
+    public void test_longDuration() {
+        int historyIndex = Integer.parseInt(PROPERTY_MANAGER.getProperty("historyIndex"));
+        switch (historyIndex) {
+            case 5:
+                TimerUtils.sleep(3000);
+                break;
+            case 10:
+                TimerUtils.sleep(5000);
+                break;
+            default:
+                TimerUtils.sleep(2000);
+                break;
+        }
+        ASSERT.assertTrue(true);
+    }
+
+    @Test
+    public void test_multipleFailureAspects() {
+        int historyIndex = Integer.parseInt(PROPERTY_MANAGER.getProperty("historyIndex"));
+        switch (historyIndex) {
+            case 5:
+                ASSERT.assertTrue(false);
+                break;
+            case 7:
+            case 10:
+                ASSERT.assertEquals(42, 7);
+                break;
+            default:
+                ASSERT.assertEquals(1, 2);
+                break;
+        }
+    }
+
+    @Test
+    public void test_addedLater() {
+        ASSERT.assertTrue(true);
+    }
+
+    @Test
+    public void test_removedLater() {
+        ASSERT.assertTrue(true);
+    }
+}

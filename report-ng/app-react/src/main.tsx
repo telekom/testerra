9429143474f65c9f82487925782c54f7e95929ad
sena <selina.natschke@t-systems.com>
2026-02-11 07:56:41 +0100

    replace react-query with tanstack/react-query

--- report-ng/app-react/src/main.tsx
+++ report-ng/app-react/src/main.tsx
@@ -1,7 +1,7 @@
 import {createRoot} from 'react-dom/client'
 
 import React from "react"
-import {QueryClient, QueryClientProvider} from "react-query";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {RouterProvider} from "react-router-dom";
 import {router} from "./router/mainRouter.tsx";
 import { StyledEngineProvider } from "@mui/material";

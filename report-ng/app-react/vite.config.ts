82a211b97d25f9496bb793e56324536c8f492568
Martin Großmann <github@martymaximus.de>
2026-07-03 11:07:20 +0200

    Chunk js files in prod build

--- report-ng/app-react/vite.config.ts
+++ report-ng/app-react/vite.config.ts
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     base: './',
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    react: ['react', 'react-dom', 'react-router-dom'],
+                    mui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+                    echarts: ['echarts', 'echarts-for-react'],
+                },
+            },
+        },
+    },
 })
